590b0fcb6f0df8cd66c647a6105e9e2ee5d7f0a1
ShadowDancer <PrzemekOnak@gmail.com>
2014-10-27 19:10:33 +0100

    Final version

--- Zajecia1/Zajecia1/Zajecia1.asm
+++ Zajecia1/Zajecia1/Zajecia1.asm
@@ -1,102 +1,77 @@
-/*
- * AssemblerApplication3.asm
- *
- *  Created: 2014-10-22 17:39:45
- *   Author: student
- */
-
- #include "m32def.inc"
-
-
- // rejestry do komentowania
-
-
-
-
- /*
- .CSEG // pocz¹tek segmentu kodu
- LDI R18, 1 // load immidiet, wczytuje 1 do rejsetru r18
-LOOP: // label, numer instrukcji
- INC R16 // inkrementacja o 1 rejestru r16
- ADD R17, R18 // dodaje rejestry
-RJMP LOOP // skacze do labela (ew. do instrukcji nr xx)
-*/
-
-/*
-.DSEG // segment danych, nie mo¿na inicjalizowa zmiennych
+.DSEG
 .ORG 0x01FF // w tym miejscu pamieci zaczyna sie segment
 
-.EQU LEN = 128 // sta³a
- tab: .BYTE LEN // rozmiar 4 bajty
- tab1: .BYTE LEN
-
-
-.CSEG
-
-LDI XL, LOW(tab) // low - nizszy bajt
-LDI XH, HIGH(tab) // hih - wyzszy bajt
-LD R16, X
-*/
-
-
-.DSEG // segment danych, nie mo¿na inicjalizowa zmiennych
-.ORG 0x01FF // w tym miejscu pamieci zaczyna sie segment
-
-.EQU LEN = 128 // sta³a
+.EQU LEN = 0 // stala
  tab: .BYTE LEN // rozmiar 4 bajty
  tab1: .BYTE LEN
 
 .CSEG
 CLC
 
+; Rejestr X, Y - adresy dodawanych bajtów
+; Rejestr Z - ile dodawan jeszcze nalezy wykonac 
+; rejestr 18 - czy w ostatnim dodawaniu by³o ustawione carry
 
+LDI ZL, LOW(LEN)    ; wczytuje do rejestru z dlugosc
+LDI ZH, HIGH(LEN)   ; cd
+
+; konczenie programu dla dlugosci 0
+CPI  ZL, 0 ; porownaj nizszy bajt z 0, jesli jest rozny od zera to zacznij petle
+BRNE START
+CPI ZH, 0 ; j.w wyzszy bajt
+BRNE START
+RJMP END ; jezeli LEN == 0 to zakoncz program
+
+LDI R18, 0 ; w rejestrze bedzie przechowywana flaga carry po adc
+
+START:
+
+; wczytywanie danych
 LDI XL, LOW(tab) // low - nizszy bajt
 LDI XH, HIGH(tab) // hih - wyzszy bajt
 
 LDI YL, LOW(tab1) // low - nizszy bajt
 LDI YH, HIGH(tab1) // hih - wyzszy bajt
 
-LDI ZL, LOW(LEN)    ; wczytuje do rejestru z d³ugoœc
-LDI ZH, HIGH(LEN)   ; cd
-
-; konczenie programu dla dlugosci 0
-CPI  ZL, 0 ; porównaj nizszy bajt z 0, jesli jest rozny od zera to zacznij petle
-BRNE LOOP:
-CPI ZH, 0 ; j.w wyzszy bajt
-BRNE LOOP:
-RJMP END ; jezeli LEN == 0 to zakoncz program
-
-
-
-
 LOOP:
 
+CPI R18, 0 ; sprawdz, czy carry byla ustawiona w poprzednim dodawaniu
 
+BRNE CARRY_SET; carry powinno byc ustawione
 
-; wczytaj mlodsze czesci liczb do rejestru
+; carry powyno byc wyzerowane
+CLC
+RJMP CARRY_AFTER_SET
+
+CARRY_SET:
+SEC
+
+CARRY_AFTER_SET:
+
+; zaladuj kolejne bajty
 LD R16, X+
-LD R17, YL
+LD R17, Y
 
 ADC R17,R16 ; dodaj
-ST Y, R17
 
+; zapisz wynik
+ST Y+, R17
 
-SBIW Z, 1 ; na koncu petli zdekrementuj dlugosc, aby odejmowac poprzednia komorke pamieci
+; zapamietaj, czy carry bylo ustawione
+BRCC NOT_CARRY
 
-; sprawdz czy liczba sie skonczyla, jesli nie to kontynuuj
-CPI  ZL, 0 ; porównaj nizszy bajt z 0, jesli jest rozny od zera to zacznij petle
-BRNE LOOP:
-CPI ZH, 0 ; j.w wyzszy bajt
-BRNE LOOP:
-RJMP END ; jezeli LEN == 0 to zakoncz program
+LDI R18, 1
+RJMP AFTER_CARRY_MEM
 
-END: RJMP END ; koniec programu
+NOT_CARRY:
+LDI R18, 0
 
+AFTER_CARRY_MEM:
 
+SBIW Z, 1 ; zmniejszamy liczbe dodawan do wykonania
+BRBS 1, END ; jesli zostalo 0 dodwan do wykonania to skoncz
 
-
-// steorwanie dobre
-LD R16, X+ // inkrementuje x po wyszstkim
-LD R17, Y
-ADC R17, R16 // wynik zostaje w R17
-ST Y+, R17 // inkrementuje Y po wysztki 
+RJMP LOOP ; jesli nie, to kontynuuj
+	
+END: 
+RJMP END ; koniec programu
